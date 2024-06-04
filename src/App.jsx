669ca6d8897baa4dc8e888532b87b6055e13479b
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-04 19:34:13 +0500

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <>
+    <BrowserRouter>
      <Navbar/>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      {/* <Route path="/about" element={} />
+      <Route path="/team" element={} />
+      <Route path="/services" element={} />
+      <Route path="/project" element={} />
+      <Route path="/blog" element={} />
+      <Route path="/contact" element={} /> */}
+      </Routes>
      <Footer/>
+    </BrowserRouter>
     </>
   )
 }
