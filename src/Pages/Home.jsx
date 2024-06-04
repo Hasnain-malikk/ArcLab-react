bad0856466c1790ced45902ec3449825f5f4ec80
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-04 23:35:38 +0500

    footer completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,15 @@ const Home = () => {
   return (
     <div style={{height:"150vh",background:"pink"}}>
       This is me Home section
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
+      <div>dadsadasdas</div>
     </div>
   )
 }
