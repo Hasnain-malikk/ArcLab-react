9aae3c23f016927b838b3a23419eb55731bdb283
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-04 19:51:05 +0500

    mavbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Footer from './Components/Footer/Footer'
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import About from './Pages/About'
+import Team from './Pages/Team'
+import Services from './Pages/Services'
+import Project from './Pages/Project'
+import Contact from './Pages/Contact'
+import Blog from './Pages/Blog'
 
 function App() {
   return (
@@ -11,12 +17,12 @@ function App() {
      <Navbar/>
      <Routes>
       <Route path="/" element={<Home/>} />
-      {/* <Route path="/about" element={} />
-      <Route path="/team" element={} />
-      <Route path="/services" element={} />
-      <Route path="/project" element={} />
-      <Route path="/blog" element={} />
-      <Route path="/contact" element={} /> */}
+      <Route path="/about" element={<About/>} />
+      <Route path="/team" element={<Team/>} />
+      <Route path="/services" element={<Services/>} />
+      <Route path="/project" element={<Project/>} />
+      <Route path="/blog" element={<Blog/>} />
+      <Route path="/contact" element={<Contact/>} />
       </Routes>
      <Footer/>
     </BrowserRouter>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      this is me navbar
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link className='brand' to="#">Hepta</Link>
+      </div>
+      <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <Link to="/">Home</Link>
+        <Link to="/hotels">Hotels</Link>
+        <Link to="/aboutus">About Us</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/news">News</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
