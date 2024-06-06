79d3700e87980a067dd10bc8881ea6a8759d6889
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-06 11:07:27 +0500

    main2 updated

--- src/Components/Main/Main2.jsx
+++ src/Components/Main/Main2.jsx
@@ -9,7 +9,7 @@ const Main2 = () => {
     <div className='main2'>
      <div className="main2-content">
       <div className="main2-breadcrumb">
-        <Link className='link' to="/">HOME</Link><span className='greater' ><FontAwesomeIcon icon={faGreaterThan} /></span><span style={{fontSize:"14px",fontWeight:"500"}} >ABOUT US</span><span className='greater' ><FontAwesomeIcon icon={faGreaterThan} /></span>
+        <Link className='link' to="/">HOME</Link><span><FontAwesomeIcon className='greater' icon={faGreaterThan} /></span><span style={{fontSize:"14px",fontWeight:"500"}} >ABOUT US</span><span><FontAwesomeIcon className='greater'  icon={faGreaterThan} /></span>
       </div>
       <div className="main2-heading">About Us</div>
      </div>
