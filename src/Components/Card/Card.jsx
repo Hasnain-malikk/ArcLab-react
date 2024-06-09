94b78c4ce523b4d667a53fd7b422633fca78c173
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-09 16:19:26 +0500

    gsap added to card component

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,20 +1,125 @@
 import React from 'react'
 import "./Card.css"
-import system from "/ReactJs/arclab-react/src/assets/system.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGears } from '@fortawesome/free-solid-svg-icons';
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Card = () => {
+
+    useGSAP(()=>{
+        gsap.from('.effect1',{
+          y:50,
+          duration:0.35,
+          delay:0.3,
+          opacity:0,
+          scrollTrigger:{
+            trigger:".effect1",
+            scroller:"body",
+            // markers:true,
+            start:"top 92%"
+          }
+        })
+      })
+      useGSAP(()=>{
+        gsap.from('.effect2',{
+          y:50,
+          duration:0.35,
+          delay:0.35,
+          opacity:0,
+          scrollTrigger:{
+            trigger:".effect2",
+            scroller:"body",
+            // markers:true,
+            start:"top 92%"
+          }
+        })
+      })
+      useGSAP(()=>{
+        gsap.from('.effect3',{
+          y:50,
+          duration:0.35,
+          delay:0.4,
+          opacity:0,
+          scrollTrigger:{
+            trigger:".effect3",
+            scroller:"body",
+            // markers:true,
+            start:"top 92%"
+          }
+        })
+      })
+      useGSAP(()=>{
+        gsap.from('.effect4',{
+          y:50,
+          duration:0.35,
+          delay:0.45,
+          opacity:0,
+          scrollTrigger:{
+            trigger:".effect4",
+            scroller:"body",
+            // markers:true,
+            start:"top 92%"
+          }
+        })
+      })
+      
   return (
     <div className='card'>
-      <div className="c2">
+      <div className="c2" id='a'>
+        <div className="effect1">
+
         <div className="card-pic">
-            <img src={system} alt="" />
+        <FontAwesomeIcon className='icon' icon={faGears} />
         </div>
         <div className="card-title">Design Process</div>
-        <div className="card-paragraph"></div>
+        <div className="card-paragraph">
+        A small river named Duden flows by their place and supplies it with the necessary regelialia.
+        </div>
       </div>
-      <div className="c1"></div>
-      <div className="c2"></div>
-      <div className="c1"></div>
+        </div>
+      <div className="c2" id='b'>
+        <div className="effect2">
+
+      <div className="card-pic">
+        <FontAwesomeIcon className='icon' icon={faPeopleGroup} />
+        </div>
+        <div className="card-title">Supervision</div>
+        <div className="card-paragraph">
+        A small river named Duden flows by their place and supplies it with the necessary regelialia.
+        </div>
+      </div>
+        </div>
+      <div className="c2" id='c'>
+        <div className="effect3">
+
+      <div className="card-pic">
+      <FontAwesomeIcon className='icon' icon={faBriefcase} />
+        </div>
+        <div className="card-title">Budget Planning</div>
+        <div className="card-paragraph">
+        A small river named Duden flows by their place and supplies it with the necessary regelialia.
+        </div>
+      </div>
+        </div>
+      <div className="c2" id='d'>
+        <div className="effect4">
+
+      <div className="card-pic">
+      <FontAwesomeIcon className='icon' icon={faHouseChimney} />
+        </div>
+        <div className="card-title">Build a Home For You</div>
+        <div className="card-paragraph">
+        A small river named Duden flows by their place and supplies it with the necessary regelialia.
+        </div>
+      </div>
+        </div>
     </div>
   )
 }
