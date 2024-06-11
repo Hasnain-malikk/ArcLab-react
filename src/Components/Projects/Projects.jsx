62339c52ef483a7d4582192c9eb6f588f6255872
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-12 00:19:17 +0500

    projects updated

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,7 +4,14 @@ import "./Projects.css"
 const Projects = () => {
   return (
     <div className='projects'>
-      <div className="projects-heading"></div>
+      <div className="projects-heading">
+        <div className="projects-head">
+            <div className="line"></div>
+            <span className="main1">PROJECTS</span>
+            <div className="line"></div>
+        </div>
+        <div className="projects-main-head">Our Unique Latest Projects</div>
+      </div>
       <div className="projects-1"></div>
     </div>
   )
