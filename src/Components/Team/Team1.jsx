9bac95fa0453dc7df0b43b2b9d3bdce02aa894ac
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-20 20:47:04 +0500

    card-added

---
+++ src/Components/Team/Team1.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Team1.css"
+
+const Team1 = () => {
+  return (
+    <div className='team1' >  
+      <div className="person1">
+        <div className="person-img">
+          <div className="team1-person-name">
+            <div className="team1-name">
+            <div className="first-name">PENNY</div>
+            <div className="last-name">ROBIONSON</div>
+            </div>
+          </div>
+        </div>
+        <div className="person-job">ARCHITECT</div>
+      </div>
+      <div className="person1"></div>
+      <div className="person1"></div>
+      <div className="person1"></div>
+    </div>
+  )
+}
+
+export default Team1
--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main2 from '../Components/Main/Main2'
+import Team1 from '../Components/Team/Team1'
 
 const Team = () => {
   return (
     <div>
       <Main2 heading="Architect" page="ARCHITECT" />
+      <Team1/>
     </div>
   )
 }
