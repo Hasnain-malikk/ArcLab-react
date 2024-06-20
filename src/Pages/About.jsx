7a2a75ce2adb0332250b1bf9d67da262f18db689
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-20 15:15:01 +0500

    counter added to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main2 from '../Components/Main/Main2'
 import Form from '../Components/Form/Form'
+import Counter from '../Components/Counter/Counter'
 
 const About = () => {
   return (
     <div>
       <Main2 heading="About Us" page="ABOUT US"/>
       <Form/>
+      <Counter/>
     </div>
   )
 }
