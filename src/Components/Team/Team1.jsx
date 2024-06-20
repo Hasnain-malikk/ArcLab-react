43d7d96d0044f2ca76dda8c72fb42ff1749e79af
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-20 20:57:51 +0500

    team1 complete

--- src/Components/Team/Team1.jsx
+++ src/Components/Team/Team1.jsx
@@ -15,9 +15,39 @@ const Team1 = () => {
         </div>
         <div className="person-job">ARCHITECT</div>
       </div>
-      <div className="person1"></div>
-      <div className="person1"></div>
-      <div className="person1"></div>
+      <div className="person1">
+      <div className="person-img1">
+          <div className="team1-person-name">
+            <div className="team1-name">
+            <div className="first-name">JHON</div>
+            <div className="last-name">ARRON</div>
+            </div>
+          </div>
+        </div>
+        <div className="person-job">ARCHITECT</div>
+      </div>
+      <div className="person1">
+      <div className="person-img2">
+          <div className="team1-person-name">
+            <div className="team1-name">
+            <div className="first-name">SANSA</div>
+            <div className="last-name">STARK</div>
+            </div>
+          </div>
+        </div>
+        <div className="person-job">ARCHITECT</div>
+      </div>
+      <div className="person1">
+      <div className="person-img3">
+          <div className="team1-person-name">
+            <div className="team1-name">
+            <div className="first-name">JAIME</div>
+            <div className="last-name">LANNISTER</div>
+            </div>
+          </div>
+        </div>
+        <div className="person-job">ARCHITECT</div>
+      </div>
     </div>
   )
 }
