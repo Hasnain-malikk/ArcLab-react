4e61146487e841ca9b7dd38ecf9753f39b8a3c36
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-06-22 11:52:50 +0500

    contact heading added

---
+++ src/Components/Heading/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Heading.css"
+
+const Heading = () => {
+  return (
+    <div className='contact-headingg'>
+      If you got any questions please do not hesitate to send us a message 
+    </div>
+  )
+}
+
+export default Heading
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main2 from '../Components/Main/Main2'
 import ContactForm from '../Components/ContactForm/ContactForm'
+import Heading from '../Components/Heading/Heading'
 
 const Contact = () => {
   return (
     <div>
       <Main2 heading="Contact us" page="CONTACT US" />
       <ContactForm/>
+      <Heading/>
     </div>
   )
 }
